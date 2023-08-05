59052e4d51efb3abcca28de2986653d44e5e185d
Qtekfun <qtekfun@gmail.com>
2023-08-05 12:17:10 +0200

    chore: update ami to amz linux 2023

---
+++ imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = data.aws_ami.amazon_linux
+  id = "ami-0f34c5ae932e6f0e4"
+}
--- instances.tf
+++ instances.tf
@@ -1,21 +1,9 @@
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
 
   tags = {
